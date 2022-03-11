ec0cd71c1e387f6b80beb5140a0c3fd33fd2ca95
Dewi Asmita <mytaloebis@gmail.com>
2022-03-11 16:24:23 +0700

    Percobaan Var dan Val

---
+++ src/main/kotlin/4_Var_dan_Val.kt
@@ -0,0 +1,18 @@
+fun main() {
+    //var adalah variabel yang nilainya dapat dirubah
+    var varVariabel: Int
+    varVariabel = 56 //menetapkan nilai pertama ke varVariabel
+    println("Pemberian nilai pertama pada varVariabel = $varVariabel")
+    varVariabel = 78 //menetapkan nilai kedua ke varVariabel
+    println("Pemberian nilai kedua pada varVariabel = $varVariabel")
+
+    //val adalah variabel yang nilainya tidak dapat diubah
+    var varVariabell: Int
+    varVariabell = 56
+    println("Pemberian nilai pertama pada varVariabel = $varVariabell")
+    varVariabell = 78
+    println("Pemberian nilai kedua pada varVariabel = $varVariabell")
+    val phi = 3.14
+
+
+}
