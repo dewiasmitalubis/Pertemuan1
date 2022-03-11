ef6caebafbd3652fbb47ccad66d77b21ba3f635d
Dewi Asmita <mytaloebis@gmail.com>
2022-03-11 16:18:07 +0700

    Percobaan Operator

---
+++ src/main/kotlin/3_Variabel.kt
@@ -0,0 +1,9 @@
+fun main() {
+    //Tipe system yang dinamis tidak memerlukan adanya deklarasi tipe data pada sebuah variabel
+    var dynamicVar = 2
+    println("contoh dynamic variabel = $dynamicVar")
+
+    //Tipe system yang statis memerlukan adanya deklarasi tipe data pada sebuah variabel
+    var staticVar: Char = 'A'
+    println("contoh static variabel = $staticVar")
+}
