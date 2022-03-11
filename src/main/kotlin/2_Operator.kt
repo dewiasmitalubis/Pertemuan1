11e98a392069143b1f5ae5122d7e5781a6c41456
Dewi Asmita <mytaloebis@gmail.com>
2022-03-11 16:08:48 +0700

    Percobaan Operator

---
+++ src/main/kotlin/2_Operator.kt
@@ -0,0 +1,16 @@
+fun main() {
+    //komentar 1 baris
+    /*
+    1 baris atau lebih
+     */
+    /**
+     * kotlin DOC --> baris ini untuk mendokumentasikan kode program
+     */
+
+    var a = 7
+    var b = 6
+    var c = (a++ + b++)/2.0
+    println(c + b)
+
+
+}
