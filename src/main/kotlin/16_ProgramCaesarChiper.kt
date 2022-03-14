e3ec318c0e1a512a7a34ddfee4b1ce4d1b968779
Dewi Asmita <mytaloebis@gmail.com>
2022-03-14 12:50:28 +0700

    Percobaan Progam Chaesar Chiper

---
+++ src/main/kotlin/16_ProgramCaesarChiper.kt
@@ -0,0 +1,22 @@
+fun main() {
+    //Program ini mengenkripsi sebuah input text String yang diberikan kedalam karakter yang dinaikan atau diturunkan
+    //The Caesar cipher
+    // variable initialization
+    val s = "blackholesarewheregoddividedbyzero"
+    println("Original message: $s")
+    var message = ""
+    var shift = 1
+    // loop iterating over characters
+    for (c in s) {
+        var i = c.toInt()
+        i += shift
+        if (i > 'z'.toInt()) {
+            i -= 26
+        }
+        val char = i.toChar()
+        message += char
+    }
+    // printing
+    println("Encrypted message: $message")
+
+}
