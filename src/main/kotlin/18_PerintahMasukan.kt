12933a7d3b2ee271102ec41614508f6df4d99098
Dewi Asmita <mytaloebis@gmail.com>
2022-03-14 12:59:17 +0700

    Percobaan Perintah Masukan di Kotlin

---
+++ src/main/kotlin/18_PerintahMasukan.kt
@@ -0,0 +1,10 @@
+fun main() {
+    //Parrot program
+    println("Hi Saya Dewi Asmita Lubis, dari prodi Rekayasa Perangkat Lunak");
+    println("Ketikkan sesuatu: ");
+    var input: String
+    input = readLine()!!
+    var output: String
+    output = input + ", " + input + "!"
+    println(output)
+}
